b3b9627d8a04d1ac18a0bdb278dfffa07b5e810d
AlhanoufIB <Alhanoufib@hotmail.com>
2026-01-23 21:34:12 +0300

    ADD DONE

---
+++ JS/add.js
@@ -0,0 +1,51 @@
+
+
+function addIngredient() {
+    const ingredientsDiv = document.getElementById('Ingredients');
+  const row=document.createElement('div');
+  row.className='ingredient-row';
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = 'ingredient';
+    input.placeholder = 'Name';
+    input.required = true;
+   const quantityInput = document.createElement('input');
+    quantityInput.type = 'text';
+    quantityInput.name = 'quantity';
+    quantityInput.placeholder = 'Quantity';
+    quantityInput.required = true;
+
+
+    row.appendChild(input);
+    row.appendChild(quantityInput);
+    ingredientsDiv.appendChild(row);
+
+}
+
+
+
+
+
+
+
+
+function addInstruction(){
+    const instructionsDiv = document.getElementById('Instructions');
+    const input = document.createElement('input');
+    input.type = 'text';
+    input.name = 'instruction';
+    input.placeholder = 'Next Step';
+    input.style.display = 'block';
+    input.style.marginTop = '8px';
+    input.required = true;
+    instructionsDiv.appendChild(input);
+}
+function redirecttoRecipes(event) {
+    event.preventDefault();
+    alert("Recipe added successfully!");
+    window.location.href = "index.html";
+   
+}
+document.addEventListener("DOMContentLoaded", function () {
+    addIngredient();
+});
