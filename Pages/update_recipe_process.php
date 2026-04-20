87b5dfbaadf783667dfad039c0df6982c9f42eaa
Latifah-AL <latifahalmousa@gmail.com>
2026-04-20 21:00:03 +0300

    edit video

--- Pages/update_recipe_process.php
+++ Pages/update_recipe_process.php
@@ -5,15 +5,11 @@ ini_set('display_errors',1);
 
 include 'db_connect.php';
 
-
 $recipeID = $_POST['recipeID'];
-
 $name = $_POST['recipeName'];
 $category = $_POST['Category'];
 $description = $_POST['Description'];
 $url = $_POST['URL'];
-
-
 
 $sql = "SELECT * FROM recipe WHERE RecipeID = $recipeID";
 $result = $conn->query($sql);
@@ -22,33 +18,35 @@ $recipe = $result->fetch_assoc();
 $photoName = $recipe['PhotoFileName'];
 $videoName = $recipe['VideoPathName'];
 
+$isFile = !empty($recipe['VideoPathName']) && !str_starts_with($recipe['VideoPathName'], 'http');
 
-
+// Handle photo
 if(!empty($_FILES['RecipePhoto']['name'])){
-
-$photoName = $_FILES['RecipePhoto']['name'];
-$tempName = $_FILES['RecipePhoto']['tmp_name'];
-
-move_uploaded_file($tempName, "../images/" . $photoName);
+    if(!empty($recipe['PhotoFileName'])){
+        unlink('../images/' . $recipe['PhotoFileName']);
+    }
+    $photoName = $_FILES['RecipePhoto']['name'];
+    $tempName = $_FILES['RecipePhoto']['tmp_name'];
+    move_uploaded_file($tempName, "../images/" . $photoName);
 }
 
-
+// Handle video
 if(!empty($_FILES['VideoOrURL']['name'])){
-
-$videoName = $_FILES['VideoOrURL']['name'];
-$tempVideo = $_FILES['VideoOrURL']['tmp_name'];
-
-move_uploaded_file($tempVideo, "../videos/" . $videoName);
-
+    if($isFile){
+        unlink('../videos/' . $recipe['VideoPathName']);
+    }
+    $videoName = $_FILES['VideoOrURL']['name'];
+    $tempVideo = $_FILES['VideoOrURL']['tmp_name'];
+    move_uploaded_file($tempVideo, "../videos/" . $videoName);
 }
 elseif(!empty($url)){
-
-$videoName = $url;
-
+    if($isFile){
+        unlink('../videos/' . $recipe['VideoPathName']);
+    }
+    $videoName = $url;
 }
 
-
-
+// Update recipe
 $sql = "UPDATE recipe 
 SET Name='$name',
 CategoryID='$category',
@@ -59,41 +57,28 @@ WHERE RecipeID=$recipeID";
 
 $conn->query($sql);
 
-
+// Update ingredients
 $conn->query("DELETE FROM ingredients WHERE RecipeID=$recipeID");
 
-
-
 if(isset($_POST['ingredientName'])){
-
-foreach($_POST['ingredientName'] as $i => $ingredient){
-
-$quantity = $_POST['ingredientQuantity'][$i];
-
-$conn->query("INSERT INTO ingredients (RecipeID, ingredientName, ingredientQuantity)
-VALUES ('$recipeID','$ingredient','$quantity')");
-}
+    foreach($_POST['ingredientName'] as $i => $ingredient){
+        $quantity = $_POST['ingredientQuantity'][$i];
+        $conn->query("INSERT INTO ingredients (RecipeID, ingredientName, ingredientQuantity)
+        VALUES ('$recipeID','$ingredient','$quantity')");
+    }
 }
 
-
-
+// Update instructions
 $conn->query("DELETE FROM instructions WHERE RecipeID=$recipeID");
 
-
-
 if(isset($_POST['Instruction'])){
-
-foreach($_POST['Instruction'] as $step => $instruction){
-
-$order = $step + 1;
-
-$conn->query("INSERT INTO instructions (RecipeID, Step, StepOrder)
-VALUES ('$recipeID','$instruction','$order')");
+    foreach($_POST['Instruction'] as $step => $instruction){
+        $order = $step + 1;
+        $conn->query("INSERT INTO instructions (RecipeID, Step, StepOrder)
+        VALUES ('$recipeID','$instruction','$order')");
+    }
 }
-}
-
 
 header("Location: MyRecipes.php");
 exit();
-
 ?>
